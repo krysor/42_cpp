e277a5c5ff4569a7334778adefb4db45f4f7c2ad
Krystian Kaczorowski <kkaczoro@mi-r1-p3.s19.be>
2023-08-22 16:08:02 +0200

    think finished ex00

--- cpp08/ex00/Sources/main.cpp
+++ cpp08/ex00/Sources/main.cpp
@@ -1,18 +1,44 @@
 #include "easyfind.hpp"
+#include <array>
 #include <vector>
+#include <deque>
+#include <forward_list>
+#include <list>
 #include <iostream>
 
-#define SIZE 10
+#define SIZE 1000
 
 int main( void ) {
-	std::vector<int> v(SIZE);
+
+	std::array<int, SIZE>	a;
+	std::vector<int>		v(SIZE);
+	std::deque<int>			d(SIZE);
+	std::forward_list<int>	fl;
+	std::list<int>			l;	
 
 	srand(time(NULL));
 	for (int i = 0; i < SIZE; i++) {
+		a[i] = i;
 		v[i] = i;
+		d[i] = i;
+		fl.push_front(i);
+		l.push_back(i);
 	}
-	
-	std::cout << easyfind(v, 0) - v.begin() << std::endl;
+	a[SIZE / 4] = 42;
+	v[SIZE / 3] = 42;
+	d[SIZE / 2] = 42;
+	fl.pop_front();
+	fl.push_front(42);
+	l.pop_back();
+	l.push_back(42);
+
+	std::cout << "number found:" << std::endl;
+   
+	std::cout << "array : " << easyfind(a, 42) - a.begin() << std::endl;
+	std::cout << "vector: " << easyfind(v, 42) - v.begin() << std::endl;
+	std::cout << "deque : " << easyfind(d, 42) - d.begin() << std::endl;
+	std::cout << "flist : " << *easyfind(fl, 42) << std::endl;
+	std::cout << "list  : " << *easyfind(l, 42) << std::endl;
 
 	return (EXIT_SUCCESS);
 }
