162d314a669d18dd107744f1bb3b9953ca7a4b0a
Krystian Kaczorowski <kkaczoro@mi-r7-p3.s19.be>
2023-08-25 13:57:11 +0200

    cpp08/ex02: almost compiling, need to add iterators

--- cpp08/ex02/Includes/MutantStack.hpp
+++ cpp08/ex02/Includes/MutantStack.hpp
@@ -3,30 +3,30 @@
 #define __MUTANTSTACK_HPP__
 
 #include <vector>
-#include <iterator>
-#include "UI.hpp"
 
 template<class T> 
 class MutantStack {
 
 private:
 	
-		std::vector<T>		stack;
+	std::vector<T>	stack;
 
 public:
 	
-	MutantStack( void );
-	explicit MutantStack( const Container& cont = Container() );
-	MutantStack( const MutantStack& other );
-	MutantStack&	operator=( const MutantStack& other );
-	~MutantStack( void );
+	MutantStack( void )						: stack(0) 				{}
+	MutantStack( const MutantStack& other ) : stack(other.stack)	{}
+	~MutantStack( void )											{}
+	MutantStack&	operator=( const MutantStack& other ) {
+		stack = other.stack;
+		return (*this);
+	}
 
-	T&					top( void ) const;
-	bool				empty( void ) const;
-	unsigned long int	size( void ) const;
-	void				push( const T& value );
-	void				pop( void );
-		
+	T&		top( void )		const	{ return *stack.end()			; }
+	bool	empty( void )	const	{ return !size() ? true : false	; }
+	size_t	size( void )	const	{ return stack.size()			; }
+	void	push( const T& value )	{ stack.push_back(value)		; }
+	void	pop( void )				{ stack.pop_back()				; }
+
 };
 
 
