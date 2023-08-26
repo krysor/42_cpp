c871c4ee7addd8e8b862e2fc2aa465b75884f560
Krystian Kaczorowski <kkaczoro@mi-r1-p3.s19.be>
2023-08-26 17:34:05 +0200

    cpp09/ex00: saving work before a quick break

--- cpp09/ex00/Includes/BitcoinExchange.hpp
+++ cpp09/ex00/Includes/BitcoinExchange.hpp
@@ -2,6 +2,14 @@
 #ifndef BITCOINEXCHANGE_HPP
 #define BITCOINEXCHANGE_HPP
 
+#include <iostream>
+#include <fstream>
+#include <map>
 
+#define	NAMEDATABASE "data.csv"
+
+bool	extractDataBase( std::map<std::string, float>& dataBase );
+bool	parseLine(std::string& line,
+				  std::map<std::string, float>& dataBase);
 
 #endif
--- cpp09/ex00/Sources/BitcoinExchange.cpp
+++ cpp09/ex00/Sources/BitcoinExchange.cpp
@@ -1,2 +1,32 @@
 #include "BitcoinExchange.hpp"
 
+bool	parseLine(std::string& line,
+				  std::map<std::string, float>& dataBase)
+{
+	(void)line;
+	(void)dataBase;
+	return (true);
+}
+
+bool	extractDataBase( std::map<std::string, float>& dataBase )
+{
+	std::ifstream	streamDataBase;
+	std::string		line;
+	
+	streamDataBase.open(NAMEDATABASE, std::ios::in);
+	if (streamDataBase == NULL)
+	{
+		std::cout << "Error: could not open data.csv file." << std::endl;
+		return (false);
+	}
+	while (std::getline(streamDataBase, line))
+	{
+		if (parseLine(line, dataBase) == false)
+		{
+			streamDataBase.close();
+			return (false);
+		}
+	}
+	streamDataBase.close();
+	return (true);
+}
